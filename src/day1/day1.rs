24bb3e9c747b66dcd706d036e3aff9589931218d
Eirik Rolland Enger <eirroleng@gmail.com>
2021-12-18 02:08:40 +0100

    :tada: Init rust repo

---
+++ src/day1/day1.rs
@@ -0,0 +1,3 @@
+pub fn count_increment() {
+    println!("This should count the number of times we see increments.")
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod day1;
+
+fn main() {
+    println!("Hello, world!");
+    day1::day1::count_increment();
+}
